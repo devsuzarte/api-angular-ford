4628ae8bcdbda57cb87c0e0d70385ab573ea3d5d
BenSuzarte <ben.dantassuzarte@gmail.com>
2025-04-11 21:26:06 -0300

    feat: cors is available for any request origin

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const cors = require("cors")
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
